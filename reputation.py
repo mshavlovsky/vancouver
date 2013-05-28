315fae8c3a8123bca0c89afca3dd308ce8f87a57
Luca de Alfaro <luca@dealfaro.org>
2013-05-28 11:12:50 -0700

    One version of median.

--- reputation.py
+++ reputation.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python
 
 import numpy as np
+import unittest
+
 
 # Do we debias grades?
 DEBIAS = True
@@ -25,6 +28,55 @@ def aggregate(v, weights=None):
         return np.average(v, weights=weights)
     else:
         return np.average(v)
+
+
+def median_aggregate(values, weights=None):
+    if len(values) == 1:
+        return values[0]
+    if weights is None:
+        weights = np.ones(len(values))
+    # Sorts. 
+    vv = []
+    for i in range(len(values)):
+        if weights[i] > 0:
+            vv.append((values[i], weights[i]))
+    if len(vv) == 0:
+        return values[0]
+    if len(vv) == 1:
+        x, _ = vv[0]
+        return x
+    vv.sort()
+    v = np.array([x for x, _ in vv])
+    w = np.array([y for _, y in vv])
+    print 'v', v, 'w', w
+    # At this point, the values are sorted, they all have non-zero weight,
+    # and there are at least two values.
+    half = np.sum(w) / 2.0
+    below = 0.0
+    i = 0
+    while i < len(v) and below + w[i] < half:
+        below += w[i]
+        i += 1
+    print 'i', i, 'half', half, 'below', below
+    if half < below + 0.5 * w[i]:
+        print 'below'
+        if i == 0:
+            return v[0]
+        else:
+            alpha = half - below + 0.5 * w[i - 1]
+            beta = below + 0.5 * w[i] - half
+            print 'alpha', alpha, 'beta', beta
+            return (beta * v[i - 1] + alpha * v[i]) / (alpha + beta)
+    else:
+        print 'above'
+        if i == len(v) - 1:
+            print 'last'
+            return v[i]
+        else:
+            alpha = half - below - 0.5 * w[i]
+            beta = below + w[i] - half + 0.5 * w[i + 1]
+            print 'alpha', alpha, 'beta', beta
+            return (beta * v[i] + alpha * v[i + 1]) / (alpha + beta)
 
 
 class Msg():
@@ -136,4 +188,31 @@ def evaluate_items(graph, n_iterations=10):
     # Does the final aggregation step.
     return aggregate_item_messages(graph)
 
+
+class TestMedian(unittest.TestCase):
+    
+    #def test_median_0(self):
+    #    values = [1.0, 3.0, 2.0]
+    #    weights = [1.0, 1.0, 1.0]
+    #    m = median_aggregate(values, weights=weights)
+    #    self.assertAlmostEqual(m, 2.0, 4)
+
+    #def test_median_1(self):
+    #    values = [1.0, 3.0, 2.0]
+    #    weights = [1.0, 1.0, 2.0]
+    #    m = median_aggregate(values, weights=weights)
+    #    self.assertAlmostEqual(m, 2.0, 4)
+
+    def test_median_2(self):
+        values = [1.0, 3.0, 2.0]
+        weights = [1.0, 2.0, 1.0]
+        m = median_aggregate(values, weights=weights)
+        self.assertAlmostEqual(m, 2.5, 4)
+
+
+
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    unittest.main()
         
