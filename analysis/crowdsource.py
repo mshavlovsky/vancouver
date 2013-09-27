bc7f1c229b4d6b2371927af3921f240fa5a35eda
Michael Shavlovsky <mshavlovsky@gmail.com>
2013-09-26 21:23:52 -0700

    Items have difficulty level now.

--- analysis/crowdsource.py
+++ analysis/crowdsource.py
@@ -16,14 +16,18 @@ BIAS_STDEV = 0.0
 EVAL_STDEV = 0.4
 FRACTION_BAD = 0.2
 GAMMA_SHAPE = 2
-N_ITERATIONS = 10
+ITEMS_DIFFICULTY = 1
+ITEMS_FRACTION_BAD = 0.2
+N_ITERATIONS = 100
 DO_PLOTS = False
 
-def eval_quality(values):
-    diffs = [values[it] - it.q for it in items]
+def eval_quality(values, items, only_good=False):
+    if only_good:
+        items = [it for it in items if it.difficulty == 0]
+    diffs = [values[it] - it.true_quality for it in items]
     stdev = np.std(diffs)
     a = [values[it] for it in items]
-    b = [it.q for it in items]
+    b = [it.true_quality for it in items]
     r = np.corrcoef(a, b)[1, 0]
     return stdev, r
 
@@ -39,24 +43,24 @@ for i in range(N_ITERATIONS):
     users = [user_model.User(bias_stdev=BIAS_STDEV, eval_stdev=EVAL_STDEV, mode='gamma',
                              gamma_shape=GAMMA_SHAPE, frac=FRACTION_BAD)
              for u in range(N_USERS)]
-    items = [item_model.Item() for i in range(N_ITEMS)]
+    items = [item_model.Item(frac=ITEMS_FRACTION_BAD, difficulty=ITEMS_DIFFICULTY) for i in range(N_ITEMS)]
     graph = graph_builder.Graph(items, users, reviews=N_REVIEWS)
     # Evaluates this according to simple average.
     values_via_avg = average_voting.evaluate_items(graph)
-    av, ac = eval_quality(values_via_avg)
+    av, ac = eval_quality(values_via_avg, items)
     #print "  Via average: ", av, ac
     avs.append(av)
     acs.append(ac)
     # Evaluates this according to the reputation system.
     values_via_rep = reputation_instrumented.evaluate_items(graph, do_plots=DO_PLOTS)
-    rv, rc = eval_quality(values_via_rep)
+    rv, rc = eval_quality(values_via_rep, items)
     #print "  Via reputation:", rv, rc
     rvs.append(rv)
     rcs.append(rc)
     # Evaluates this according to the repuation systme using mle estimator for users' variance
     graph.use_mle = True
     values_via_rep_mle = reputation_instrumented.evaluate_items(graph, do_plots=DO_PLOTS)
-    mle_v, mle_c = eval_quality(values_via_rep_mle)
+    mle_v, mle_c = eval_quality(values_via_rep_mle, items)
     stdev_mle.append(mle_v)
     corr_mle.append(mle_c)
 
--- analysis/item_model.py
+++ analysis/item_model.py
@@ -3,12 +3,22 @@ import numpy.random as npr
 
 
 class Item:
-    def __init__(self, stdev=1.0):
+    def __init__(self, stdev=1.0, frac=0.1, difficulty=0.0):
         self._stdev = stdev
-        self.q = npr.normal(loc=0.0, scale=stdev)
+        if npr.uniform() < frac:
+            self.difficulty = difficulty
+        else:
+            self.difficulty = 0.0
+        self.true_quality = npr.normal(scale=stdev)
         self.users = []
         self.grade = None
         
+    @property
+    def q(self):
+        if self.difficulty <= 0:
+            return self.true_quality
+        else:
+            return self.true_quality + npr.normal(scale=self.difficulty)
+
     def add_user(self, user):
         self.users.append(user)
-        
