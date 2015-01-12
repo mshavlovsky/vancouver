898684bff0bdd98063e7c60f809225bb82f41c91
Michael Shavlovsky <mshavlovsky@gmail.com>
2015-01-12 13:34:41 -0800

    Updated user model

--- analysis/user_model.py
+++ analysis/user_model.py
@@ -5,14 +5,20 @@ import numpy.random as npr
 class User:
     
     def __init__(self, bias_stdev=0.2, eval_stdev=0.2, mode='pareto', frac=0.1, 
-                 pareto_shape=1.4, gamma_shape=3):
+                 pareto_shape=1.4, gamma_shape=3, true_bias=None, true_precision=None, is_instructor=False):
         """Initializes the precision and bias of a user.  Useful only for simulation."""
         # Chooses the bias of the user
-        self.true_bias = 0
-        if bias_stdev > 0:
-            self.true_bias = npr.normal(scale=bias_stdev)
+        if true_bias is None:
+            self.true_bias = 0
+            if bias_stdev > 0:
+                self.true_bias = npr.normal(scale=bias_stdev)
+        else:
+            self.true_bias = true_bias
+        self.is_instructor = is_instructor
         # Chooses the variance of the user.
-        if mode == 'bimodal':
+        if not true_precision is None:
+            self.prec = true_precision
+        elif mode == 'bimodal':
             # 10% of the students are responsible for 90% of the trouble,
             # where 10% is the fraction.
             # This code keeps the standard deviation as specified, but explains
@@ -48,4 +54,12 @@ class User:
         obviously."""
         self.items.append(item)
         self.grade[item] = self.judge(item)
+
+    def is_instructors_neighbour(self):
+        """For debugging purposes only"""
+        for it in self.items:
+            for neighbour in it.users:
+                if neighbour != self and neighbour.is_instructor:
+                    return True
+        return False
     
